b80cccba161be60f4a22325a0b7bb1389a4bab14
vikramltts <78849038+vikramltts@users.noreply.github.com>
2021-03-09 08:30:40 +0530

    Update MyConnection.java

--- Day3/miniproject/configuration/MyConnection.java
+++ Day3/miniproject/configuration/MyConnection.java
@@ -1,14 +1,17 @@
 package com.ltts.Configuration;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
+	
+	import java.sql.Connection;
+	import java.sql.DriverManager;
+	import java.sql.SQLException;
 
-public class MyConnection {
-	public static Connection getConnection() throws Exception
-	{
-		Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/roshan","root","root");
-		
-		return con;
-		
+	public class MyConnection {
+		public static Connection getConnection() throws SQLException
+		{
+			Connection c=DriverManager.getConnection("jdbc:mysql://localhost:3306/miniproject","root","root");
+			
+			return c;
+			
+		}
+
 	}
-}
